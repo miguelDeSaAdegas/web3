908bf98e15f8475fae1b41d4652fe0cdb3faa3bb
miguelDeSaAdegas <miguel.desaadegas@gmail.com>
2023-09-15 11:11:10 +0200

    tuto

---
+++ exercices/module1/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+const Hello = (props) => {
+
+  console.log(props)
+  return (
+    <>
+      <p>
+
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </>
+  )
+}
+
+const App = () => {
+  const friends = [
+    { name: 'Peter', age: 4 },
+    { name: 'Maya', age: 10 },
+  ]
+
+  return (
+    <div>
+      <p>{friends[0].name} {friends[0].age}</p>
+      <p>{friends[1].name} {friends[1].age}</p>
+    </div>
+  )
+}
+
+export default App
