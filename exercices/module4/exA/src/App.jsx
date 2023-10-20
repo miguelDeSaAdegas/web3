43810e64ed92396d5f5a6290c1834cfaa757205b
Miguel De Sa Adegas <miguel.desaadegas@student.vinci.be>
2023-10-20 12:43:54 +0200

    fin semaine 4

--- exercices/module4/exA/src/App.jsx
+++ exercices/module4/exA/src/App.jsx
@@ -1,21 +1,65 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
+import axios from 'axios'
+
+
+const PersonRendering = ({persons}) => {
+  return (
+    <div>
+      <ul>
+        {persons.map((person, index) => (
+          <li key={index}>{person.name} {person.number}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }
+
+  useEffect(hook, [])
+  console.log('render', persons.length, 'persons')
+
 
   const handleAddName = (event) => {
-    console.log(event.target.value);
     setNewName(event.target.value);
+  }
+
+  const handleAddNumber = (event) => {
+    setNewNumber(event.target.value);
   }
 
   const handleAddPerson = (event) => {
     event.preventDefault();
-    setPersons(persons.concat(newName));
-    setNewName('');
+    if (persons.find(e => e.name.includes(newName) && e.number.includes(newNumber)) !== undefined) {
+      alert(`${newName} is already added to the phonebook`)
+    } else {
+      const personObject = {
+        name: newName,
+        number: newNumber
+      }
+      
+      axios
+        .post('http://localhost:3001/persons', personObject)
+        .then(response => {
+          setPersons(persons.concat(personObject));
+          setNewName('');
+          setNewNumber('');
+        })
+    }
   }
 
   return (
@@ -24,8 +68,15 @@ const App = () => {
       <form onSubmit={handleAddPerson}>
         <div>
         <input
+          placeholder='name'
           value={newName}
           onChange={handleAddName}
+        />
+        <br/>
+        <input
+          placeholder='number'
+          value={newNumber}
+          onChange={handleAddNumber}
         />
         </div>
         <div>
@@ -33,9 +84,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person, index) => (
-          <p key={index}>{person.name}</p>
-        ))}
+      <PersonRendering persons={persons}/>
     </div>
   )
 }
